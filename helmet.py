21f0bacb747257c89a520c07c39aab78f2de782f
Nic Doye <nic.doye@alfresco.com>
2017-09-04 16:39:48 +0100

    Now with functions.

--- helmet.py
+++ helmet.py
@@ -5,29 +5,85 @@ import requests
 import string
 import re
 
-charts = 'https://kubernetes-charts.storage.googleapis.com/'
-r = requests.get(charts)
+# Could get command line
+default_repo_url = 'https://kubernetes-charts.storage.googleapis.com/'
 
-tree = etree.XML(r.content)
 
-latest_version = {}
-for chart in tree.findall('{http://doc.s3.amazonaws.com/2006-03-01}Contents'):
-    chart_key = chart.find('{http://doc.s3.amazonaws.com/2006-03-01}Key').text
-    chart_parts = re.split('-\d', chart_key, maxsplit=1)
-    if len(chart_parts) == 2:
-        software = chart_parts[0]
-        version = re.sub( software + '-' , '', chart_key, count = 1 )
-        version = re.sub( '.tgz$', '', version, count = 1)
+def update_latest_version(latest_version_dict, software, version):
+    """
+    Could write as if not in dict or if version >...
+    """
+    # Check if already in dictionary
+    if software in latest_version_dict:
+        # Check if this a newer version
+        if version > latest_version_dict[software]:
+            latest_version_dict[software] = version
+    else:
+        latest_version_dict[software] = version
 
-        # Check if already in dictionary
-        if software in latest_version:
-            # Check if this a newer version
-            if version > latest_version[software]:
-                latest_version[software] = version
-        else:
-            latest_version[software] = version
-        
-        
-for software in latest_version:        
-    print('{} {}'.format(software, latest_version[software]))
 
+def is_valid_entry(split_chart_entry):
+    """
+    If the Key doesn't isn't foo-bar-n.m.l.tgz then, it's not software.
+    Although we are only checking for the '-n' part
+    """
+    return(len(split_chart_entry) == 2)
+
+
+def get_charts(repo_url):
+    """
+    Read XML from repo
+    """
+    r = requests.get(repo_url)
+    return(r.content)
+
+
+def get_chart_xml_tree(raw_xml):
+    """
+    Turn XML intro tree object
+    """
+    return(etree.XML(raw_xml))
+
+
+def get_software_version_pair(split_chart_entry, chart_entry):
+    """
+    Turn valid string into pair
+    """
+    software = split_chart_entry[0]
+    version = re.sub(software + '-', '', chart_entry, count=1)
+    version = re.sub('.tgz$', '', version, count=1)
+    return(software, version)
+
+
+def read_charts(tree):
+    """
+    Loop through all the XML looking for the latest version of each chart
+    """
+    latest_version_dict = {}
+    for chart in tree.findall('{http://doc.s3.amazonaws.com/2006-03-01}Contents'):
+        chart_entry = chart.find(
+            '{http://doc.s3.amazonaws.com/2006-03-01}Key').text
+        split_chart_entry = re.split('-\d', chart_entry, maxsplit=1)
+        if is_valid_entry(split_chart_entry):
+            software, version = get_software_version_pair(
+                split_chart_entry, chart_entry)
+            update_latest_version(latest_version_dict, software, version)
+    return(latest_version_dict)
+
+
+def dump_dict(str_str_dict):
+    """
+    Print all elements of the dictionary
+    """
+    for key, value in str_str_dict.items():
+        print('{} {}'.format(key, str_str_dict[key]))
+
+
+def main():
+    tree = get_chart_xml_tree(get_charts(default_repo_url))
+    latest_version_dict = read_charts(tree)
+    dump_dict(latest_version_dict)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
